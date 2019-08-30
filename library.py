48891dcf6d0b6a496c199c2175859ec177943e76
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-08-30 13:36:50 +0530

    defined library functions

--- library.py
+++ library.py
@@ -3,10 +3,13 @@ import shapely
 class Path:
 	def __init__(self, source):
 		self.start = source
+		path = [self.start]
 
 	def visiblity_graph(self):
 		vis_graph = []
 		#
 		#
 		return vis_graph
-		
+
+	def add_point(pt):
+		self.path.append[pt]
